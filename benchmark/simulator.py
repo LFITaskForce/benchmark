f3532a008a06efa92e888d281314dee8ba4ff998
Johann Brehmer <mail@johannbrehmer.de>
2019-03-21 11:12:59 -0400

    Added log_prob() function to interface

--- benchmark/simulator.py
+++ benchmark/simulator.py
@@ -4,5 +4,12 @@ class Simulator:
     def forward(self, inputs):
         raise NotImplementedError()
 
+    def log_prob(self, inputs, outputs):
+        raise IntractableException
+
     def __call__(self, inputs):
         return self.forward(inputs)
+
+
+class IntractableException(Exception):
+    pass
