4eca2a43462b3e1c5842426bb5f32f9bcb851d3b
Joeri Hermans <joeri@joerihermans.com>
2019-03-21 16:35:17 +0100

    Add authors

---
+++ lfibenchmarks/__init__.py
@@ -0,0 +1,20 @@
+"""
+Benchmarking problems for likelihood-free inference.
+"""
+
+__version__ = "0.0.1"
+
+# Ordered alphabetically
+__author__ = [
+    "Danley Hsu",
+    "Henri Pesonen",
+    "Jan Matthis",
+    "Joeri Hermans",
+    "Johann Brehmer",
+    "Umberto Simola"]
+
+# Global namespace.
+from pyro.util import set_rng_seed
+
+# Default configuration.
+set_rng_seed(0)
--- lfibenchmarks/simulator.py
+++ lfibenchmarks/simulator.py
@@ -1,3 +1,5 @@
+
+
 class Simulator:
     """ Abstract simulator interface. """
 
