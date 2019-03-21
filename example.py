adc3d387ca3204cd3178d8347f8edb1787d820cc
Jan-Matthis <mail@jan-matthis.de>
2019-03-21 10:29:39 -0400

    update pyro example

--- example.py
+++ example.py
@@ -5,9 +5,18 @@ import pyro.distributions as dist
 
 
 def simulator(parameters):
-    return parameters
+    w = torch.tensor([0.5, 0.5])
 
-input = torch.tensor([1,2])
+    n = pyro.sample("noise", dist.Normal(0., 1.0))
+
+    return torch.dot(parameters, w) + n
+
+
+input = torch.tensor([1., 2.])
+
+#simulator(input)
+
 trace = pyro.poutine.trace(simulator).get_trace(input)
 
-print(trace)
+
+trace(dir(trace))
