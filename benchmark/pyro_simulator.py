Fixed joint score calculation by detaching latents

benchmark/pyro_simulator.py
@@ -67,6 +67,7 @@ class PyroSimulator(Simulator):
     def _calculate_joint_score(self, trace, inputs):
         score = 0.
         for dist, z, _ in self._get_branchings(trace):
+            z = z.detach()
             log_p = dist.log_prob(z)
             try:
                 score = score + grad(
@@ -74,7 +75,7 @@ class PyroSimulator(Simulator):
                     inputs,
                     grad_outputs=torch.ones_like(log_p.data),
                     only_inputs=True,
-                    create_graph=False,
+                    retain_graph=True,
                 )[0]
             except RuntimeError:
                 # This can happen when individual distributions do not depend on the input params

examples/calculate_joint_score_and_ratio.py
@@ -14,15 +14,15 @@ class SuperSimpleSimulator(PyroSimulator):
 
 sim = SuperSimpleSimulator()
 
-n_samples = 10000
+n_samples = 100000
 theta = torch.tensor([[0.3, 1.5] for _ in range(n_samples)])
 theta_ref = torch.tensor([[0., 1.] for _ in range(n_samples)])
 
 x, joint_score, joint_log_ratio = sim.augmented_data(theta, theta_ref, None)
 
-print(x)  # Seems okay
-print(joint_score)  # The first component is always 0. It seems that the mean of the Gaussian does not affect log_prob?
-print(joint_log_ratio)  # Seems to work
+print(x)
+print(joint_score)
+print(joint_log_ratio)
 
-print(torch.mean(joint_score, dim=0))  # The second component does not average out to 0, as it should...
-print(torch.mean(torch.exp(joint_log_ratio), dim=0))  # Seems to work
+print(torch.mean(joint_score, dim=0))
+print(torch.mean(torch.exp(joint_log_ratio), dim=0))

examples/wrong_score_mwe.py
@@ -8,7 +8,7 @@ mean = torch.tensor([1. for _ in range(n_samples)], requires_grad=True)
 std = torch.tensor([2. for _ in range(n_samples)], requires_grad=True)
 
 normal = Normal(mean, std)
-x = pyro.sample("normal", normal)
+x = pyro.sample("normal", normal).detach()
 
 log_prob = normal.log_prob(x)
 
@@ -19,7 +19,6 @@ score_mean = grad(
     only_inputs=True,
     retain_graph=True,
 )[0]
-
 score_std = grad(
     log_prob,
     std,