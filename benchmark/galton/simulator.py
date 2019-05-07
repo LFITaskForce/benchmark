4aa8053fd5b53ec92ae3b0c293f2f05913b485ca
SebastianMacaluso <bueseba@gmail.com>
2019-05-07 16:12:11 -0400

    Updated Galton Board

--- benchmark/galton/simulator.py
+++ benchmark/galton/simulator.py
@@ -1,11 +1,12 @@
 #
 # Generalized Galton board example.
 #
-
+import sys
 import numpy as np
 import torch
+from torch import nn
 import pyro
-from benchmark import PyroSimulator
+from benchmark.pyro_simulator import PyroSimulator
 
 
 class Simulator(PyroSimulator):
@@ -23,90 +24,152 @@ class Simulator(PyroSimulator):
     start_pos = starting position (default = n_nails / 2)
     """
 
-    def __init__(self, n_rows=20, n_nails=31, start_pos=None):
+    def __init__(self, n_rows=5, n_nails=31, start_pos=None):
         super(Simulator, self).__init__()
-        self.n_rows = torch.tensor(n_rows)
+
+        self.n_rows = torch.tensor(n_rows, dtype=torch.float)
         if self.n_rows < 2:
             raise ValueError('Must be at least two rows.')
+
         self.n_nails = torch.tensor(n_nails)
         if self.n_nails < 2:
             raise ValueError('Must be at least two nails.')
+
         if start_pos == None:
-            self.start_pos = self.n_nails // 2
+            self.start_pos = self.n_nails // 2 # We choose the nail at the center
         else:
             self.start_pos = start_pos
+
         if self.start_pos < 0 or self.start_pos > self.n_nails:
             raise ValueError('Must start within bounds of [0, n_nails]')
 
-    def nail_positions(self, theta, level=None, nail=None):
-        if level is None or nail is None:
-            level = torch.arange(self.n_rows, dtype=torch.float).expand(self.n_nails, self.n_rows)
-            nail = torch.arange(self.n_nails, dtype=torch.float).expand(self.n_rows, self.n_nails)
 
-        level_rel = 1. * torch.tensor(level, dtype=torch.float) / (self.n_rows - 1)
+
+
+
+    def nail_positions(self, theta, level=None, nail=None):
+
+        if level is None or nail is None:
+            level = torch.arange(self.n_rows, dtype=torch.float).expand(self.n_nails, self.n_rows)# With expand we get a tensor of n_nails rows and n_rows colums.
+            print('level shape =',level.shape)
+            level=torch.transpose(level,0,-1) #We transpose it so that each level is a row. So the 1st row has all 0, the 2nd all 1, etc
+
+            nail = torch.arange(self.n_nails, dtype=torch.float).expand(self.n_rows, self.n_nails)# We get the full Galton board
+
+
+        # level=torch.tensor(level, dtype=torch.float)
+        # nail =torch.tensor(nail, dtype=torch.float)
+
+        level_rel = 1. * level / (self.n_rows - 1)
         nail_rel = 2. * nail / (self.n_nails - 1) - 1.
 
-        sigm = 1. / (1. + torch.exp(-(10*theta*nail_rel)))
+        m = nn.Sigmoid()
+        # sigm = m(torch.tensor(10*theta*nail_rel, dtype=torch.float))
+        sigm = m(10 * theta * nail_rel)
+        # print('sigm =', sigm)
+
+
+        # p(z_h,z_v,theta) - see pag. 3 of 1805.12244. This gives the probability for each value [z_h,z_v] of going left. Returns 0.5 for theta=0
         nail_positions = ((1. - torch.sin(np.pi * level_rel)) * 0.5
                           + torch.sin(np.pi * level_rel) * sigm)
 
+
         return nail_positions
 
-    def threshold(self, theta, trace):
-        begin, z = trace
-        pos = begin
-        level = 0
-        for step in z:
-            if level % 2 == 0:
-                pos[step==0] = pos[step==0]
-                pos[step!=0] = pos[step!=0] + 1
-            else:
-                pos[step==0] = pos[step==0] - 1
-                pos[step!=0] = pos[step!=0]
 
-            level += 1
 
-        tmp_nail = self.nail_positions(theta, level, pos)
 
-        if level % 2 == 1:  # for odd rows, the first and last nails are constant
-            tmp_nail[pos == 0.] = 0.0
-            tmp_nail[pos == float(self.n_nails)] = 1.0
-
-        return tmp_nail
 
     def forward(self, inputs):
         inputs = inputs.view(-1, 1)
-        num_samples = inputs.size(0)
-        theta = inputs[:, 0]
+        num_samples = inputs.shape[0]
+        theta = inputs[:, 0] # We could have input variables
+        print('Num samples = ',num_samples)
+        print('Theta = ',theta)
 
-        distribution_u = pyro.distributions.Uniform(0, 1).expand([num_samples])
+        # Define a pyro distribution
+        # distribution_u = pyro.distributions.Uniform(0, 1).expand([num_samples])
+        # dist_bern = pyro.distributions.Bernoulli(probs=a)
+
+        # print('distribution_u = ', distribution_u)
+        print('---' * 3)
 
         # Run and mine gold
         # left/right decisions are based on value of theta
         # log_pxz based on value of theta
 
-        begin = pos = torch.zeros(num_samples).fill_(self.start_pos)
-        z = []
 
-        while len(z) < self.n_rows:
-            t = self.threshold(theta, (begin, z))
-            level = len(z)
+        # begin = torch.zeros(num_samples).fill_(self.start_pos)
+        pos = torch.zeros(num_samples).fill_(self.start_pos)
 
-            # Left indicies
-            ind_list = (pyro.sample("u"+str(len(z)), distribution_u) < t) | (t == 1.0)
+        # begin = np.empty(num_samples)
+        # pos = np.empty(num_samples)
 
-            # going left
-            if level % 2 == 0:  # even rows
-                pos[ind_list] = pos[ind_list]
-                pos[~ind_list] = pos[~ind_list] + 1
-            else:  # odd rows
-                pos[ind_list] = pos[ind_list] - 1
-                pos[~ind_list] = pos[~ind_list]
+        # begin.fill(self.n_nails // 2)
+        # pos.fill(self.n_nails // 2)
 
-            tmp_z = torch.zeros(num_samples)
-            tmp_z[~ind_list] = 1
-            z.append(tmp_z)
+        # print('Begin = pos =', begin)
+        print('---' * 3)
+
+        # z = []
+        z=torch.tensor(0,dtype=torch.float)
+
+        while z < self.n_rows:
+            '''
+            Parametrization of the Galton Board
             
+            level 0 =   0   1   2   3   ...  29  30  ...
+            level 1 = 0   1   2   3   ...  29  30  ...
+            level 2 =   0   1   2   3   ...  29  30  ...
+            '''
+
+            # print('pos before calling threshold = ',pos)
+            # print('begin before calling threshold = ', begin)
+            level = z
+            print('Level = ',level)
+
+            tmp_prob = self.nail_positions(theta, level=level, nail=pos)
+
+            if level % 2 == 0:  # For the 1st nail (pos=0) the prob. of going left is 0 and for the last one (pos=n_nail) it is 1)
+                tmp_prob[pos == float(self.n_nails)] = 1.0
+            else:
+                tmp_prob[pos == 0.] = 0.0
+
+            t =tmp_prob
+
+            # t = self.threshold(theta, (begin, z))
+            # t is the probability of going left. At first z is empty so we don't change pos, and use the begin value.
+
+
+
+            # Left indices
+            dist_bern = pyro.distributions.Bernoulli(probs=t)
+
+            # print('Sample =',dist_bern.sample())
+            draw = pyro.sample("u" + str(z), dist_bern)  # We draw a number for each ball from the distribution_u
+
+
+            print('t = ', t)
+            print('pyro sample = ',draw) # We sample from the pyro distribution
+            print('---')
+
+
+            # # going left
+            print('pos before = ', pos)
+
+            # draw==1 means going left and draw==0 going right
+            if level % 2 == 0:  # even rows
+                pos[draw==0] = pos[draw==0] + 1 # We move to the right when ind_list[i]==0 (When the sampled value is greater than the prob. of going left).
+            else:  # odd rows
+                pos[draw==1] = pos[draw==1] - 1 # We move to the left when ind_list[i]==1
+
+
+            print('pos after = ', pos)
+            print('---' * 40)
+            print('---' * 40)
+
+            z+=1
+
         x = pos
 
         return x
--- examples/galton.py
+++ examples/galton.py
@@ -2,18 +2,43 @@ import numpy as np
 import torch
 import pprint
 import matplotlib.pyplot as plt
+import sys
 
-from benchmark.galton import Simulator
 
-inputs = torch.tensor(5000*[[3.]])
-simulator = Simulator()
+from benchmark.galton.simulator import Simulator
+
+# theta = torch.tensor(5000*[[3.]])
+# theta_ref = torch.tensor(5000*[[0.7]])
+
+theta = torch.tensor(5*[[3.]])
+theta_ref = torch.tensor(5*[[0.7]])
+
+
+simulator = Simulator(n_rows=20, n_nails=31)
 #simulator = Simulator(sensitivities=True)
-output = simulator(inputs)
+# output = simulator(theta)
+
+
+# print('Trace nodes =', simulator.trace(theta).nodes)
+#
+x, joint_score, joint_log_ratio = simulator.augmented_data(theta,theta, theta_ref)
+## x, joint_score, joint_log_ratio = simulator.augmented_data(theta,None, None)
+
+print('x = ', x)
+print('joint_score = ',joint_score)
+print('joint_log_ratio= ',joint_log_ratio)
+print('---'*5)
+
+
+#
+# print(torch.mean(joint_score, dim=0))
+# print(torch.mean(torch.exp(joint_log_ratio), dim=0))
 
 #printer = pprint.PrettyPrinter(indent=4)
 #printer.pprint(simulator.trace(inputs).nodes)
 
-plt.hist(output.numpy(), 31)
-plt.xlabel(r"$x$")
-plt.ylabel("Frequency")
-plt.show()
+# plt.hist(output.numpy(), 31,range=[0,31])
+# plt.xlabel(r"$x$")
+# plt.ylabel("Frequency")
+#
+# plt.show()
